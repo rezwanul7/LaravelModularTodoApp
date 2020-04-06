92e98709a4726be4bac86916b5fb833457a831dd
Rezwanul Islam <rezwanul7@gmail.com>
2020-04-06 13:58:56 +0600

    update TodoFilter

--- Modules/Todo/ModelFilters/TodoFilter.php
+++ Modules/Todo/ModelFilters/TodoFilter.php
@@ -3,9 +3,14 @@
 namespace Modules\Todo\ModelFilters;
 
 use EloquentFilter\ModelFilter;
+use Modules\Core\Traits\Filter\SearchAble;
+use Modules\Core\Traits\Filter\Sortable;
 
 class TodoFilter extends ModelFilter
 {
+    use SearchAble;
+    use Sortable;
+
     /**
      * Related Models that have ModelFilters as well as the method on the ModelFilter
      * As [relationMethod => [input_key1, input_key2]].
@@ -13,4 +18,19 @@ class TodoFilter extends ModelFilter
      * @var array
      */
     public $relations = [];
+
+    /**
+     * The attributes that are simply searchable using query as &search_keyword=keyword.
+     *
+     * @var array
+     */
+    public $searchable = ["name"];
+
+    /**
+     * The attributes that are sortable using query as &sort_by=attributeName&sort_direction=direction (i.e. asc/desc).
+     *
+     * @var array
+     */
+    public $sortable = ['created_at', 'updated_at', 'id', 'name'];
+
 }
